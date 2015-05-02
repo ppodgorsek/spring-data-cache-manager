b961a89ff622a5510a61cd65a6b1c6cb3e4c0e1c
Paul Podgorsek <ppodgorsek@users.noreply.github.com>
2015-05-02 13:24:09 +0200

    Updated the tests.

--- spring-data-cache-manager-cassandra/src/test/java/org/ppodgorsek/cache/manager/cassandra/manager/SpringDataCassandraCacheManagerTest.java
+++ spring-data-cache-manager-cassandra/src/test/java/org/ppodgorsek/cache/manager/cassandra/manager/SpringDataCassandraCacheManagerTest.java
@@ -1,26 +1,45 @@
 package org.ppodgorsek.cache.manager.cassandra.manager;
 
-import org.junit.Before;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ppodgorsek.cache.manager.cassandra.config.CassandraCacheRegionConfigurationContext;
 import org.ppodgorsek.cache.manager.core.manager.SpringDataCacheManager;
+import org.ppodgorsek.cache.manager.test.config.SpringDataCacheManagerContext;
+import org.springframework.cache.Cache;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Tests the Spring Data cache manager with Cassandra.
  *
  * @author Paul Podgorsek
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { SpringDataCacheManagerContext.class, CassandraCacheRegionConfigurationContext.class })
 public class SpringDataCassandraCacheManagerTest {
 
-	private SpringDataCacheManager springDataCacheManager = new SpringDataCacheManager();
+	@Resource
+	private SpringDataCacheManager springDataCacheManager;
 
-	@Before
-	public void initValues() {
-		springDataCacheManager.afterPropertiesSet();
+	@Test
+	public void getExistingCache() {
+
+		final Cache cache = springDataCacheManager.getCache("user");
+
+		assertNotNull("The cache should exist", cache);
 	}
 
 	@Test
-	public void test() {
+	public void getUnknownCache() {
 
+		final Cache cache = springDataCacheManager.getCache("unknown-cache");
+
+		assertNull("The cache shouldn't exist", cache);
 	}
 
 }
